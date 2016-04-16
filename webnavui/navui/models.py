aea1cd1450a8f74740a1273deb4f7587922596f1
Ivan Voras <ivoras@gmail.com>
2016-04-16 17:28:22 +0000

    Basic relay control working

--- webnavui/navui/models.py
+++ webnavui/navui/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+import bluetooth
+
+ARDUINO_ADDR = '98:23:31:90:02:70'
+ARDUINO_NAME = 'LiGhTpAtH'
+
+sock = None
+
+
+def sock_readln(sock):
+    ln = ''
+    while True:
+        ch = sock.recv(1).decode("UTF-8")
+        if ch != '\n':
+            ln += ch
+        else:
+            break
+    return ln
+
+
 class LocationType(models.Model):
     name = models.CharField(max_length=20)
     order = models.IntegerField(default=0)
@@ -16,9 +35,49 @@ class Location(models.Model):
     map_url = models.CharField(max_length=100, blank=True, null=True)
     enabled = models.BooleanField(default=True)
     order = models.IntegerField(default=0)
+    display_seconds = models.IntegerField(default=30)
+    active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+
+    def connect_sock(self, force=False):
+        global sock
+        if sock == None or force:
+            sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+            sock.connect((ARDUINO_ADDR, 1))
+
+    def turn_on_relay(self):
+        self.connect_sock()
+
+        while True:
+            try:
+                sock.sendall('BTR.HIGH %d\n' % self.relay_no)
+                resp = sock_readln(sock).strip()
+                break
+            except:
+                self.connect_sock(True)
+        
+        if resp != '+OK':
+            print(resp)
+        else:
+            print("Turned on %d" % self.relay_no)
+
+    def turn_off_relay(self):
+        self.connect_sock()
+
+        while True:
+            try:
+                sock.sendall('BTR.LOW %d\n' % self.relay_no)
+                resp = sock_readln(sock).strip()
+                break
+            except:
+                self.connect_sock(True)
+
+        if resp != '+OK':
+            print(resp)
+        else:
+            print("Turned off %d" % self.relay_no)
 
 
 class NavigationAction(models.Model):
--- webnavui/navui/views.py
+++ webnavui/navui/views.py
@@ -1,16 +1,42 @@
+import json
+import time
+import threading
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import LocationType, Location
+from .models import LocationType, Location, NavigationAction
+
 
 
 def index(req):
     ctx = {}
-
     ctx['ltypes'] = [ { 'name': lt.name, 'locations': Location.objects.filter(location_type=lt, enabled=True).order_by('order') } for lt in LocationType.objects.filter(enabled=True).order_by('order') ]
-
     return render(req, 'navui/index.html', ctx)
+
 
 @csrf_exempt
 def navclick(req):
-    pass
+    l = Location.objects.get(id=int(req.POST['lid']))
+    na = NavigationAction(location=l)
+    na.save()
+    l.active = True
+    l.save()
+
+    l.turn_on_relay()
+
+    t = threading.Thread(target=location_display_expiry_thread, args=(l,))
+    t.daemon = True
+    t.start()
+
+    return JsonResponse({ 'ok': True, 'lid': l.id, 'na': na.id })
+
+
+def location_display_expiry_thread(l):
+    print("Countdown of %d seconds to turn off relay %d" % (l.display_seconds, l.relay_no))
+    try:
+        time.sleep(l.display_seconds)
+    finally:
+        l.turn_off_relay()
+
