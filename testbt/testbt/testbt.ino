bb86702c598e01671c0fdec1466be1038f7bb461
Ivan Voras <ivoras@gmail.com>
2016-04-16 14:02:08 +0200

    Bluetooth module test

---
+++ testbt/testbt/testbt.ino
@@ -0,0 +1,25 @@
+String message; //string that stores the incoming message
+
+void setup()
+{
+  Serial.begin(9600); //set baud rate
+  delay(2000);
+  Serial.println("AT");
+  Serial.readStringUntil('\n');
+  Serial.println("AT+NAMELiGhTpAtH");
+  Serial.readStringUntil('\n');
+  Serial.setTimeout(1000);
+}
+
+void loop()
+{
+  String message = Serial.readStringUntil('\n');
+  message.trim();
+
+  if (message.length() == 0)
+    return;
+    
+  Serial.println(message);
+ }
+    
+
